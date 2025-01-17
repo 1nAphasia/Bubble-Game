64cc281ba3172ea6cb01353aac3bff88db37deec
Vista <792372447@qq.com>
2025-01-18 04:05:42 +0800

    updated 1/18 4:05

--- Assets/Scripts/BubbleBehaviour.cs
+++ Assets/Scripts/BubbleBehaviour.cs
@@ -23,7 +23,7 @@ public class BubbleBehaviour : MonoBehaviour
         capturing_enemy
     }
 
-    float bubbleExistTime=10f;
+    float bubbleExistTime=5f;
     float floatingTime=3f;
     float capturingTime=0.3f;
     Vector3 CapturedScale=new Vector3(3f,3f,3f);
@@ -45,6 +45,12 @@ public class BubbleBehaviour : MonoBehaviour
     void Update()
     {
         switch(_st){
+            case bubbleStatus.floating:
+            {
+                _rb.gravityScale=0f;
+                _rb.velocity=new Vector2(_rb.velocity.x,2.0f);
+                break;
+            }
         case bubbleStatus.carrying_enemy:
         {
             _rb.velocity=new Vector2(0,1);
@@ -68,9 +74,10 @@ public class BubbleBehaviour : MonoBehaviour
             _st=bubbleStatus.capturing_enemy;
             CapturingEnemy();
         }
-        if(other.gameObject.name=="Capsule"){
+        if(other.gameObject.name=="Player"){
             targetObject=other.gameObject;
             _st=bubbleStatus.carrying_player;
+            Debug.Log("Player Touched");
             CapturingPlayer();
         }
             
@@ -80,28 +87,36 @@ public class BubbleBehaviour : MonoBehaviour
             //Vector3 currentPosition=transform.position;
             //Vector3 v=(predictedEnemyPosition-currentPosition)/0.3f;
             //_rb.velocity=v;
+            StartCoroutine(SlowDownEnemy(0.3f));
             StartCoroutine(MorphingBubble(CapturedScale,0.3f));
             StartCoroutine(StartingBubble(0.3f));
             StartCoroutine(SetEnemyState(floatingTime,EnemyBehavior.EnemyState.Idle));
         }
 
         private void CapturingPlayer(){
-
+            StartCoroutine(MorphingBubble(CapturedScale,0.3f));
+            //StartCoroutine()
         }
         private IEnumerator StartingBubble(float delay){
+            var enemyScript=targetObject.GetComponent<EnemyBehavior>();
             yield return new WaitForSeconds(delay);
             _st=bubbleStatus.carrying_enemy;
-            var enemyScript=targetObject.GetComponent<EnemyBehavior>();
             enemyScript.currentState=EnemyBehavior.EnemyState.InBubble;
+
+        }
+
+        private IEnumerator PlayerGettingIn(float delay){
+            yield return new WaitForSeconds(delay);
+            _st=bubbleStatus.carrying_player;
+            //这里控制Player状态机的转换。
         }
 
         private IEnumerator MorphingBubble(Vector3 targetScale,float time){
             Vector3 InitialScale=transform.localScale;
-            Vector3 InitialPos=transform.position;
             float elapsedTime=0f;
             while(elapsedTime<time){
                 Vector3 EnemyPos=targetObject.transform.position;
-                transform.position=Vector3.Lerp(InitialPos,EnemyPos,elapsedTime/time);
+                transform.position=Vector3.Lerp(transform.position,EnemyPos,elapsedTime/time);
                 transform.localScale=Vector3.Lerp(InitialScale,targetScale,elapsedTime/time);
                 elapsedTime+=Time.deltaTime;
                 yield return null;
@@ -109,9 +124,21 @@ public class BubbleBehaviour : MonoBehaviour
             transform.localScale=targetScale;
         }
 
+        private IEnumerator SlowDownEnemy(float time){
+            float elapsedTime=0f;
+            float initialDrag=2f;
+            var rb=targetObject.GetComponent<Rigidbody2D>();
+            while(elapsedTime<time){
+                rb.drag = Mathf.Lerp(0, initialDrag, elapsedTime / time);
+                yield return null;
+            }
+            rb.velocity=new Vector2(0f,0f);
+            
+        }
         private IEnumerator SetEnemyState(float delay,EnemyBehavior.EnemyState EState){
             yield return new WaitForSeconds(delay);
-            targetObject.GetComponent<EnemyBehavior>().currentState=EState;
+            var enemyScript=targetObject.GetComponent<EnemyBehavior>();
+            enemyScript.currentState=EState;
         }
 
 }
--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -11,14 +11,23 @@ public class EnemyBehavior : MonoBehaviour
         Jumping,
         InBubble
     }
+    public List<Vector2> PatrolPoints;
+    public float moveSpeed=3f;
+    private int currentPointIndex=0;
+    private float step;
+    public LayerMask groundLayer;
     public EnemyState currentState;
     private Rigidbody2D _rb;
+    private GameObject _player;
 
     // Start is called before the first frame update
     void Start()
     {
+        PatrolPoints.Add(new Vector2(2f,-4.28f));
+        PatrolPoints.Add(new Vector2(8f,-4.28f));
         currentState=EnemyState.Idle;
         _rb=GetComponent<Rigidbody2D>();
+        _player=GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -33,10 +42,31 @@ public class EnemyBehavior : MonoBehaviour
             }
             case EnemyState.Idle:
             {
-                
-                _rb.gravityScale=0;
+                _rb.gravityScale=1;
+                if(Vector3.Distance(transform.position,_player.transform.position)<=10&&IsGrounded()){
+                    currentState=EnemyState.Patrolling;
+                }
+                break;
+            }
+            case EnemyState.Patrolling:
+            {
+                Patrol();
                 break;
             }
         }
+    }
+    void Patrol(){
+        if(PatrolPoints.Count==0)
+        return;
+        Vector2 moveDir=(PatrolPoints[currentPointIndex]-new Vector2(transform.position.x,transform.position.y)).normalized;
+        _rb.velocity=moveDir*moveSpeed;
+        if(Vector2.Distance(PatrolPoints[currentPointIndex],new Vector2(transform.position.x,transform.position.y))<0.2f){
+            currentPointIndex = (currentPointIndex + 1) % PatrolPoints.Count;
+        }
+    }
+
+    bool IsGrounded(){
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.3f, groundLayer);
+        return hit.collider != null;
     }
 }
